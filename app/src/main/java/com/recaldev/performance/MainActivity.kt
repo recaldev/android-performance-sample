023f05432ce4a4a3a93770c18bb2496b1475854c
Diego Recalde <difereto89@gmail.com>
2019-12-14 12:18:17 -0500

    Created a list of Globers with Overdraw Problem.
    Signed-off-by: Diego Recalde <diego.recalde@globant.com>

--- app/src/main/java/com/recaldev/performance/MainActivity.kt
+++ app/src/main/java/com/recaldev/performance/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.recaldev.performance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity(), MainPresenter.View {
 
     private lateinit var presenter: MainPresenter
 
+    private lateinit var recycler: RecyclerView
+    private lateinit var adapter: MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter =  MainPresenter()
-        presenter.init()
+        recycler = findViewById(R.id.recycler)
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        adapter = MainAdapter()
+        recycler.adapter = adapter
+
+        presenter = MainPresenter()
+        presenter.init(this)
+    }
+
+    override fun loadData(globers: List<Glober>) {
+        adapter.globers = globers
     }
 }
